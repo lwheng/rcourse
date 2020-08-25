45f45d0f1f458f5a00e78d92955fedc0a94dabc7
Low-Wee Heng <alpha116@gmail.com>
2020-08-25 20:24:14 +0800

    Section 01

---
+++ tutorial03.R
@@ -0,0 +1,20 @@
+#Logical:
+#TRUE T
+#FALSE 5
+
+4 < 5
+10 > 100
+4 == 5
+
+result <- 4 < 5
+result
+typeof(result)
+
+result2 <- !result
+result2
+
+result | result2
+result & result2
+
+isTRUE(result)
+isTRUE(result2)
